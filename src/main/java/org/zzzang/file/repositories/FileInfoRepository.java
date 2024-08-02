ee43cd5cc10fb8f3712c1aadab39ecdbac8449c1
KIMJEONGHYEON1016 <dkemfdk4@naver.com>
2024-08-02 11:38:54 +0900

    file upload

--- src/main/java/org/zzzang/file/repositories/FileInfoRepository.java
+++ src/main/java/org/zzzang/file/repositories/FileInfoRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.zzzang.file.entities.FileInfo;
 
-public interface fileInfoRepository extends JpaRepository<FileInfo, Long>, QuerydslPredicateExecutor<FileInfo> {
+public interface FileInfoRepository extends JpaRepository<FileInfo, Long>, QuerydslPredicateExecutor<FileInfo> {
 
 }
--- src/main/java/org/zzzang/file/services/FileUploadService.java
+++ src/main/java/org/zzzang/file/services/FileUploadService.java
@@ -1,18 +1,29 @@
 package org.zzzang.file.services;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 import org.zzzang.file.entities.FileInfo;
+import org.zzzang.file.repositories.FileInfoRepository;
+import org.zzzang.global.configs.FileProperties;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@EnableConfigurationProperties(FileProperties.class)
 public class FileUploadService {
 
-    public void upload(MultipartFile[] files, String gid, String location) {
+    private final FileInfoRepository fileInfoRepository;
+    private final FileProperties properties;
+
+    public List<FileInfo> upload(MultipartFile[] files, String gid, String location) {
         /**
          * 1. 파일 정보 저장
          * 2. 파일을 서버로 이동
@@ -21,6 +32,8 @@ public class FileUploadService {
          */
 
         gid = StringUtils.hasText(gid) ? gid : UUID.randomUUID().toString();
+
+        List<FileInfo> uploadedFiles = new ArrayList<>();
 
         // 1. 파일 정보 저장
         for (MultipartFile file : files) {
@@ -35,7 +48,30 @@ public class FileUploadService {
                     .contentType(contentType)
                     .extension(extension)
                     .build();
+
+            fileInfoRepository.saveAndFlush(fileInfo);
+
+            // 2. 파일을 서버로 이동
+            long seq = fileInfo.getSeq();
+            String uploadDir = properties.getPath() + "/" + (seq % 10L);
+            File dir =  new File(uploadDir);
+            if (!dir.exists() || !dir.isDirectory()) {
+                dir.mkdir();
+            }
+
+            String uploadPath = uploadDir + "/" + seq + extension;
+            try {
+                file.transferTo(new File(uploadPath));
+                uploadedFiles.add(fileInfo);    // 업로드 성공 파일 정보
+            } catch (IOException e) {
+                e.printStackTrace();
+                // 파일 이동 실패시 정보 삭제
+                fileInfoRepository.delete(fileInfo);
+                fileInfoRepository.flush();
+            }
         }
+
+        return uploadedFiles;
     }
 }
 
