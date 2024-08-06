2256561a16bb9dfea53dbba1d21d8e1cd1a91d94
KIMJEONGHYEON1016 <dkemfdk4@naver.com>
2024-08-06 11:54:27 +0900

    file controller

--- src/main/java/org/zzzang/file/controllers/FileController.java
+++ src/main/java/org/zzzang/file/controllers/FileController.java
@@ -6,6 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.zzzang.file.entities.FileInfo;
+import org.zzzang.file.services.FileDeleteService;
+import org.zzzang.file.services.FileDownloadService;
+import org.zzzang.file.services.FileInfoService;
 import org.zzzang.file.services.FileUploadService;
 import org.zzzang.global.exceptions.RestExceptionProcessor;
 import org.zzzang.global.rests.JSONData;
@@ -19,6 +22,9 @@ import java.util.List;
 public class FileController implements RestExceptionProcessor {
 
     private final FileUploadService uploadService;
+    private final FileDownloadService downloadService;
+    private final FileInfoService infoService;
+    private final FileDeleteService deleteService;
 
     @PostMapping("/upload")
     public ResponseEntity<JSONData> upload(@RequestPart("file") MultipartFile[] files, @RequestParam(name="gid", required = false) String gid, @RequestParam(name="location", required = false) String location) {
@@ -30,5 +36,41 @@ public class FileController implements RestExceptionProcessor {
 
         return ResponseEntity.status(status).body(data);
     }
+
+    @GetMapping("/download/{seq}")
+    public void download(@PathVariable("seq") Long seq) {
+        downloadService.download(seq);
+    }
+
+    @DeleteMapping("/delete/{seq}")
+    public JSONData delete(@PathVariable("seq") Long seq) {
+       FileInfo data = deleteService.delete(seq);
+
+       return new JSONData(data);
+    }
+
+    @DeleteMapping("/deletes/{gid}")
+    public JSONData deletes(@PathVariable String gid, @RequestParam(name="location", required = false) String location) {
+        List<FileInfo> items = deleteService.delete(gid, location);
+
+        return new JSONData(items);
+    }
+
+    // 개별 조회
+    @GetMapping("/info/{seq}")
+    public JSONData get(@PathVariable("seq") Long seq) {
+        FileInfo data = infoService.get(seq);
+
+        return new JSONData(data);
+    }
+
+    // 목록 조회
+    @GetMapping("/list/{gid}")
+    public JSONData get(@PathVariable("gid") String gid, @RequestParam(name="location", required = false) String location) {
+        List<FileInfo> items = infoService.getList(gid, location);
+
+        return new JSONData(items);
+    }
+
 }
 
--- src/main/java/org/zzzang/file/services/FileDeleteService.java
+++ src/main/java/org/zzzang/file/services/FileDeleteService.java
@@ -3,10 +3,15 @@ package org.zzzang.file.services;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
+import org.zzzang.file.constants.FileStatus;
 import org.zzzang.file.entities.FileInfo;
 import org.zzzang.file.repositories.FileInfoRepository;
+import org.zzzang.global.exceptions.UnAuthorizedException;
 import org.zzzang.member.MemberUtil;
 import org.zzzang.member.entities.Member;
+
+import java.io.File;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -22,8 +27,35 @@ public class FileDeleteService {
 
         Member member = memberUtil.getMember();
         if (!memberUtil.isAdmin() && StringUtils.hasText(createBy) && memberUtil.isLogin() && !member.getEmail().equals(createBy)) {
-            throw new
+            throw new UnAuthorizedException();
         }
+
+        // 파일 삭제
+        File file = new File(filePath);
+        if (file.exists()) {
+            file.delete();
+        }
+
+        // 파일 정보 삭제
+        infoRepository.delete(data);
+        infoRepository.flush();
+
+        return data;
+    }
+
+    public List<FileInfo> delete(String gid, String location, FileStatus status) {
+        List<FileInfo> items = infoService.getList(gid, location, status);
+        items.forEach(i -> delete(i.getSeq()));
+
+        return items;
+    }
+
+    public List<FileInfo> delete(String gid, String location) {
+        return delete(gid, location, FileStatus.All);
+    }
+
+    public List<FileInfo> delete(String gid) {
+        return delete(gid, null);
     }
 
 }
