59e865df7205ee645416413f7f58fe9a7fc2e2a5
KIMJEONGHYEON1016 <dkemfdk4@naver.com>
2024-08-06 13:43:02 +0900

    file js

--- src/main/java/org/zzzang/main/controllers/MainController.java
+++ src/main/java/org/zzzang/main/controllers/MainController.java
@@ -1,16 +1,21 @@
 package org.zzzang.main.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.zzzang.global.exceptions.ExceptionProcessor;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class MainController implements ExceptionProcessor {
 
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("addCommonScript", List.of("fileManager"));
+        model.addAttribute("addScript", List.of("test/form"));
 
         return "front/main/index";
     }
